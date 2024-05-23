dad1de071a92b9b8564c21c247b6fa665422f9d5
helymenezes <helymenezes@hotmail.com>
2024-05-23 00:25:26 -0300

    version_1.1

--- autocadastro.py
+++ autocadastro.py
@@ -10,6 +10,7 @@ cabecalho = 9
 plan_instalacao = pd.read_excel(path_excel, header= cabecalho)
 df = plan_instalacao[['Código ONE', 'Uc', 'Nº Obra']]
 df = df.replace(0, pd.NA).dropna().dropna(axis=1)
+df['Nº Obra'] = df['Nº Obra'].astype(str).str.rstrip('.0')
 
 #Alerta para inicio:
 pyautogui.alert("Aplicação esta funcionando neste nomento não interropa o computador")
@@ -24,7 +25,7 @@ time.sleep(4)
 pyautogui.write('hely')
 pyautogui.press('tab') 
 pyautogui.write('hrs500')
-time.sleep(0.5)
+time.sleep(2.5)
 pyautogui.moveTo(999,488)
 pyautogui.click(clicks = 1, button = 'left')
 time.sleep(3.5)
@@ -39,7 +40,7 @@ pyautogui.press('enter')
 # Realizando  a busca do cod UC e Numero de obrhely hrs500a:
 time.sleep(1)
 #print(pyautogui.position())
-pyautogui.moveTo(x=959, y=278, duration=0.5)
+pyautogui.moveTo(x=959, y=278, duration=1.5)
 pyautogui.click(clicks = 1, button = 'left')
 
 # Armazenar a posição inicial do mouse para retorno
@@ -48,7 +49,12 @@ start_position = pyautogui.position()
 for i, row in df.iterrows():
     codigo_one = str(row['Código ONE'])
     codigo_uc = str(row['Uc'])
-    codigo_obra = str(row['Nº Obra'])
+    codigo_obra = row['Nº Obra']
+    
+    if i == len(df) - 1:
+        pyautogui.alert(f"O último número do Código ONE é {codigo_one}")
+        break
+    
     time.sleep(2)
     pyautogui.write(codigo_one)
     time.sleep(2)
@@ -66,18 +72,30 @@ for i, row in df.iterrows():
     pyautogui.moveTo(x=1132, y=144, duration= 1.5)
     pyautogui.click(clicks = 1, button = 'left')
     pyautogui.write(codigo_uc)
-    time.sleep(10)
-    print(pyautogui.position())
+    time.sleep(2)
+    pyautogui.moveTo(x=762, y=117, duration= 1.5)
+    time.sleep(2)
+    pyautogui.click(clicks = 1, button = 'left')
+    pyautogui.write(codigo_obra)
+    time.sleep(2)
+    pyautogui.moveTo(x=738, y=89, duration= 1.5)
+    pyautogui.click(clicks = 1, button = 'left')
+    time.sleep(2)
+    pyautogui.press('enter')
+    time.sleep(2)
+    pyautogui.moveTo(x=951, y=79, duration= 1.5)
+    pyautogui.click(clicks = 1, button = 'left')
+    #print(pyautogui.position())
+    #Retornar à posição inicial
+    pyautogui.moveTo(start_position)
+    pyautogui.click(clicks = 2, button = 'left')
+    pyautogui.press('del')
     
-    # Executar cliques necessários
-    # pyautogui.moveTo(291, 311)
-    # pyautogui.click(clicks=1, button='left')
-    # pyautogui.moveTo(248, 462)
-    # pyautogui.click(clicks=1, button='left')
-    # time.sleep(2)
+
     
-    # Retornar à posição inicial
-    # pyautogui.moveTo(start_position)
+
+    
+
 
     # Limpando o campo de entrada para a próxima iteração
     # pyautogui.moveTo(x=959, y=278, duration=1)
